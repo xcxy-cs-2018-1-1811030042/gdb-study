70f67039861987f4947c1b67e7458f78c012b02e
xcxy-cs-2018-1-1811030042 <93764010@qq.com>
2020-11-30 15:26:34 +0800

    add a file:demo-01.c use test

---
+++ demo-01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    int input = 0;
+    printf("input an interger:\n");
+    scanf("%d",&input);
+    printf("the interger you input is%d\n",input);
+    return 0;
+}
